6901ff0069630247735cac56ee19fbcc12f5a861
eiriselias <eiriselias@gmail.com>
2023-02-06 12:16:07 -0500

    barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegadorComponent } from './components/navegador/navegador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegadorComponent
   ],
   imports: [
     BrowserModule,
