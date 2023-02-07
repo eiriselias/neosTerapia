ae23bd8e7350f9458674906fc04066583e81efec
eiriselias <eiriselias@gmail.com>
2023-02-07 18:25:57 -0500

    se agrego la vision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
 import { HomeComponent } from './components/home/home.component';
 import { MisionComponent } from './components/mision/mision.component';
+import { VisionComponent } from './components/vision/vision.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavegadorComponent,
     HomeComponent,
-    MisionComponent
+    MisionComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
