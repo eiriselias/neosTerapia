10372e757926624ec0fa39e96dd766bba0dd9656
eiriselias <eiriselias@gmail.com>
2023-02-07 14:53:53 -0500

    se agrego el texto del Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegadorComponent } from './components/navegador/navegador.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavegadorComponent
+    NavegadorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
