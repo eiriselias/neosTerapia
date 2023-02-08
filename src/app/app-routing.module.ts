134141ef9961fc093cdc972bd2a796365eae88ac
eiriselias <eiriselias@gmail.com>
2023-02-07 19:35:14 -0500

    modificacion de espacios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MisionComponent } from './components/mision/mision.component';
+import { VisionComponent } from './components/vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent, pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"mision",component:MisionComponent},
+  {path:"vision",component:VisionComponent},
+  {path:"**", redirectTo:"/", pathMatch:"full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
